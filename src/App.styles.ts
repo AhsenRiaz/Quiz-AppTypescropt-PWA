3c8c15bbdea7c250edb6110473bbc337f29393f3
AhsenRiaz <64923337+AhsenRiaz@users.noreply.github.com>
2020-12-01 02:34:00 +0500

    Initial commit

---
+++ src/App.styles.ts
@@ -0,0 +1,77 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import BGImage from './images/ian-badenhorst-SuT2MhkdyBQ-unsplash.jpg';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    background-image: url(${BGImage});
+    background-size: cover;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+  }
+
+  * {
+    font-family: 'Catamaran', sans-serif;
+    box-sizing: border-box;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .score {
+    color: #56ccff;
+    font-size: 2rem;
+    margin: 0;
+    background-color:white;
+    border-radius:15px;
+    padding-left:10px;
+    padding-right:10px;
+    margin-bottom:1rem;
+  }
+
+  h1 {
+    font-family: Fascinate Inline;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    font-weight: 400;
+    background-size: 100%;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    filter: drop-shadow(2px 2px #0085a3);
+    font-size: 70px;
+    text-align: center;
+    margin: 20px;
+  }
+
+  .start, .next {
+    cursor: pointer;
+    background: linear-gradient(180deg, #ffffff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+  }
+  .start:hover{
+      border:2px solid white;
+  }
+
+  .start {
+    max-width: 200px;
+  }
+`;
